387fccd846af34e69c172cec4ccf3e5a4d0c6edf
Dmitri Nik <nikiforov.d.i@mail.ru>
2021-06-07 10:00:37 +0300

    Бот, повторяющий сообщения в телеграме

---
+++ nik_bot/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace nik_bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRunBot = new System.Windows.Forms.Button();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRunBot
+            // 
+            this.btnRunBot.Location = new System.Drawing.Point(6, 45);
+            this.btnRunBot.Name = "btnRunBot";
+            this.btnRunBot.Size = new System.Drawing.Size(118, 23);
+            this.btnRunBot.TabIndex = 0;
+            this.btnRunBot.Text = "Запустить бота";
+            this.btnRunBot.UseVisualStyleBackColor = true;
+            this.btnRunBot.Click += new System.EventHandler(this.btnRunBot_Click);
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(6, 19);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(337, 20);
+            this.txtKey.TabIndex = 1;
+            this.txtKey.Text = "1821469427:AAFQVd0nyqyBPSuwUqADef9BWOWqvHg_0r0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtKey);
+            this.groupBox1.Controls.Add(this.btnRunBot);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(349, 77);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ключ бота в telegram";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 112);
+            this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(389, 150);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRunBot;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ nik_bot/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Windows.Forms;
+
+namespace nik_bot
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bw;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.bw = new BackgroundWorker();
+            this.bw.DoWork += this.bw_DoWork;
+        }
+
+        async void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try {
+                var key = e.Argument as String;
+                var bot = new Telegram.Bot.TelegramBotClient(key);
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                await bot.SetWebhookAsync("");
+                int offset = 0;
+
+                while (true) {
+                    var updates = await bot.GetUpdatesAsync(offset);
+
+                    foreach (var update in updates) {
+                        offset = update.Id + 1;
+
+                        if (update.Type != Telegram.Bot.Types.Enums.UpdateType.Message ||
+                            update.Message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                            continue;
+
+                        var message = update.Message;
+
+                        if (message.Text == "/start") {
+                            await bot.SendTextMessageAsync(message.Chat.Id,
+                                $"Я бот-дразнилка!{Environment.NewLine}" +
+                                $"И буду повторять почти всё что ты напишешь!");
+                        }
+                        else {
+                            await bot.SendTextMessageAsync(message.Chat.Id, message.Text);
+                        }
+                    }
+                }
+            }
+            catch (Telegram.Bot.Exceptions.ApiRequestException ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btnRunBot_Click(object sender, EventArgs e)
+        {
+            var key = txtKey.Text;
+
+            if (key == "") {
+                MessageBox.Show("Прежде чем начать, введите ключ бота.");
+                return;
+            }
+
+            if (this.bw.IsBusy != true) {
+                this.bw.RunWorkerAsync(key); 
+                btnRunBot.Text = "Бот запущен...";
+            }
+        }
+    }
+}
